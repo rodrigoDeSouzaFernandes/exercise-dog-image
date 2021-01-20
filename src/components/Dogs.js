7d00c44aad6cb093c45a7112c2f478879d0e4090
Rodrigo de Souza <rdesouza2008@gmail.com>
2021-01-20 19:28:02 -0300

    exercicio

--- src/components/Dogs.js
+++ src/components/Dogs.js
@@ -1,27 +1,45 @@
 import React from 'react'
 
-class Dogs extends React.component {
+class Dogs extends React.Component {
     constructor() {
         super()
         this.state = {
-            dogUrl: ""
+            dogUrl: "",
+            dogStatus: ""
         }
+        this.dogFeth = this.dogFeth.bind(this);
     }
 
     componentDidMount(){
         this.dogFeth();
     }
+        
 
     dogFeth() {
-        fetch('https://dog.ceo/api/breeds/image/random Fetch!')
-        .then(a => console.log(a))
-    }
+         this.setState({
+            dogStatus: false
+        }, () => {fetch('https://dog.ceo/api/breeds/image/random')
+        .then(response => response.json())
+        .then(json => {
+            this.setState({
+                dogUrl: json.message,
+                dogStatus: true
+            })
+        })})
+        
+        
+    }   
 
     render(){
+        const { dogStatus } = this.state
+        if(!dogStatus){ 
+            return(<h1 className="loading">LOADING...</h1>)
+        }
         return(
             <div>
                 <h1>Random Dog</h1>
-                <img src={} />
+                <img src={this.state.dogUrl} alt="dog" />
+                <button onClick={this.dogFeth}>fetch</button>
             </div>
         );
     }
